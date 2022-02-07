9f72351d6ed3765117d596971de67a6eae2bf811
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-08 02:31:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [hidden, sethidden] = useState(true);
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id='click' onClick={()=> {
+        sethidden(false)
+      }}>Show Paragraph</button>
+      
+      { hidden? null : <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> }
+
     </div>
   );
 }
